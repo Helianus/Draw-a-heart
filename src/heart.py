f48178854f843f36c6b474673ccc01af9a95fcbe
Helianus <xinyuhelianushu@gmail.com>
2018-06-18 20:22:43 -0500

    create a double heart

---
+++ src/heart.py
@@ -0,0 +1,17 @@
+# Draw a double heart
+
+import turtle as t
+t.begin_fill()
+t.color("red","pink")
+t.left(135)
+t.forward(100)
+t.circle(-50, 180)
+t.left(90)
+t.circle(-50, 180)
+t.forward(200)
+t.circle(50, 180)
+t.right(90)
+t.circle(50, 180)
+t.forward(100)
+t.end_fill()
+t.done()
